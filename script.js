8b998ee8fdd94ead68a98fbd2de1134d9a430525
Luděk Roleček <ludek@rolecek.cz>
2023-06-06 17:50:58 +0200

    Aktualizace podkladů

---
+++ script.js
@@ -0,0 +1,81 @@
+/*
+Při řešení příkladů dodržuj následující:
+
+- prvky v DOM vybírej jakýmkoliv způsobem, ale nejlepší je přidat
+	k nim v HTML atribut id a potom vybírat prvky podle něho
+
+- vybraný prvek ukládej raději do konstant (const) než do proměnných (let)
+
+- nezapomeň dvouslovné CSS vlastnosti uvádět v camelCase podobě
+
+- hodnoty CSS vlastností v JavaScriptu jsou vždy v uvozovkách
+
+- když měníme textový obsah a nepotřebujeme v textu HTML značky,
+	tak použijeme textContent. Když HTML značky potřebujeme, musíme
+	použít innerHTML.
+*/
+
+
+// 1. Rozjedeme to ve velkém a uděláme celou stránku mnohem krásnější,
+// než je teď!
+// Vyber hlavní nadpis na stránce a změň mu JavaScriptem barvu na růžovou
+// a písmo na "cursive".
+
+
+
+
+// 2. Vyber první odstavec textu a přidej k němu pomocí JavaScriptu
+// rámeček čárkovanou čarou, 5px tlustou, zelenou barvou. Nááádhera!
+
+
+
+
+// 3. V CSS souboru přidej styl pro třídu .milenky a nastav libovolné
+// vlastnosti - já doporučuji růžovou barvu pozadí, padding 20px, červené
+// písmo.
+// JavaScriptem vyber v dokumentu třetí odstavec (kde je zmíněna Katrin
+// a Andrea) a na tento odstavec třídu "milenky" přidej.
+
+
+
+
+// 4. JavaScriptem vyber poslední odstavec a změn text uvnitř na:
+// "Jéé, ahoj Věro. Jedeš za Karlem, viď? Prosím vyřiď mu, že dnes přijdu domů pozdě."
+
+
+
+
+// 5. JavaScriptem vyber předposlední odstavec. Text odstavec zaměň
+// za stejný text, ve kterém je ale přímá řeč v úvozovkách zobrazena kurzívou.
+// (tj. vlož do odstavce ten stejný text, ale s přidanými HTML značkami)
+
+
+
+
+// 6. Obrázek šokovaného smajlíku pod vtipem vyměň za vysmátou verzi.
+// Obrázek najdeš ve složce images.
+
+
+
+
+// 7. Zablbni si :) Udělej sama/sám další změny, které tě napadnou.
+// Tipy:
+// a) přidej někam do textu odkaz, který vede na google.com.
+// JavaScriptem zaměň cíl odkazu na duckduckgo.com
+
+// b) při výměně obrázku neměň jenom obsah atributu src,
+// ale vymeň i hodnotu atributu alt (popis obrázku)
+
+// c) pokus se vybrat pomocí document.querySelector různé
+// prvky různými způsoby.Vyzkoušej si vybírání html značky,
+// třídy, id, kombinované selektory, apod.
+
+// d) zkus u prvku změnit CSS vlastnosti, které jsme
+// zatím nezkoušeli
+
+// e) vytvoř si v HTML prázdný <div>, přidej mu id
+// a zkus pomocí JavaScriptu do tohoto divu vložit
+// nadpis a odstavec textu (tj. vkládat budeš obsah,
+// ve kterém jsou HTML značky)
+
+// Hezkou zábavu 🌷
